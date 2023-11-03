6c107831934966dffba599cd07a4edb01f80d4ad
Abhishek Pratap Singh <84613735+i-apsingh@users.noreply.github.com>
2023-11-03 15:45:19 +0530

    new file

--- basic.java
+++ basic.java
@@ -8,10 +8,7 @@ public class basic {
     //switch statement
     int number =2;
     switch(number){
-      case 1: System.out.println("samosa");
-      case 2: System.out.println("burger");
-      case 3: System.out.println("mango");
-
+    
       default: System.out.println("we wake up");
 
     
