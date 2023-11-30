700f9f8ad9a3fca2fde92519625a94155269bd7c
Abhishek Pratap Singh <84613735+i-apsingh@users.noreply.github.com>
2023-11-30 09:38:07 +0000

    commit

---
+++ switchstat.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class switchstat {
+    public static void main (String args[]){
+
+        // switch statement
+        Scanner select = new Scanner(System.in);
+        System.out.println("enter day in numbers from 1 to 7");
+        int number = select.nextInt();
+
+        switch(number){
+            case 1 : System.out.println("monday \n");
+            break;
+            case 2 : System.out.println("tuesday \n");
+            break;
+            case 3 : System.out.println("wednesday \n");
+            break;
+            case 4 : System.out.println("thursday \n");
+            break;
+            case 5 : System.out.println("friday \n");
+            break;
+            case 6 : System.out.println("saturday \n");
+            break;
+            case 7 : System.out.println("sunday \n");
+            break;
+
+            default : System.out.println("enter valid number");
+      
+        }
+
+        
+    }
+}
