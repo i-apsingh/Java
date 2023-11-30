0075bae004f58d4a9e9ec4a9b026659283ba08fc
Abhishek Pratap Singh <84613735+i-apsingh@users.noreply.github.com>
2023-11-30 09:58:37 +0000

    commit

--- switchstat.java
+++ switchstat.java
@@ -3,8 +3,9 @@ public class switchstat {
     public static void main (String args[]){
         //nested switch
 
-        
+
         // switch statement
+        /* 
         Scanner select = new Scanner(System.in);
         System.out.println("enter day in numbers from 1 to 7");
         int number = select.nextInt();
@@ -26,7 +27,28 @@ public class switchstat {
             break;
 
             default : System.out.println("enter valid number");
-      
+
+        }
+        */
+
+        //code2
+
+        Scanner select = new Scanner (System.in);
+        System.out.println(" find treasure in number from 1 to 4");
+        int number = select.nextInt();
+
+        switch(number){
+            case 1 : System.out.println("samosa");
+            break;
+            case 2 : System.out.println("kachori");
+            break;
+            case 3 : System.out.println("jalebi");
+            break;
+            case 4 : System.out.println("Rasgulla");
+            break;
+
+            default : System.out.println("you are unluckey");
+            
         }
 
         
