8dad37f74297cd48a16ca2f04c19acffcc05e752
Abhishek Pratap Singh <84613735+i-apsingh@users.noreply.github.com>
2024-04-09 14:21:20 +0530

    commit

--- loops.java
+++ loops.java
@@ -1,15 +1,68 @@
 import java.util.*;
-public class loops {
-    public static void main (String args[]){
-        //while loop
-        Scanner select = new Scanner (System.in);
-        System.out.println(" how many time you want to print hello world");
-        int time = select.nextInt();
-
-        int count =1;
-        while (count<=time){
+public class loops{
+    public static void main(String[] args) {
+        
+// do while loop
+/* 
+        int counter =1;
+        do{
             System.out.println("hello world");
-            count=count+1;    
+            counter = counter +1;
+        } while(counter <=10);
+*/
+
+// for loop
+/* 
+        for (int i=1;i<=10; i++){
+            System.out.println("hello world");
         }
+*/
+
+// While Loop
+
+// sum of natural number until the user want 
+/* 
+            System.out.println("enter the numbers until you want your sum");
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int i =1;
+            int sum=0;
+            while(i<=n){
+                sum = sum+i;
+                i=i+1;
+            }
+            System.out.println(sum+ ": this is your answer");
+*/
+
+/* 
+        //Print hello world acording to user.
+        System.out.println("enter number");
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        int counter =1;
+        while (counter<=n){
+            System.out.println(counter + "");
+            counter = counter +1;
+        }
+*/
+
+        // Print numbers from 1 to 10 using while loop.
+/* 
+        int counter =1;
+        while(counter<=10){
+            System.out.println(counter);
+            counter++;
+        }
+*/
+
+        // Print hello world 100 times.
+/* 
+        int counter=0;
+        while(counter<100){
+            System.out.println("hello world");
+            counter ++;
+        }
+        System.out.println("print hello print 100 times");
+*/
     }
 }
--- temp.java
+++ temp.java
@@ -1,10 +1,6 @@
 import java.util.Scanner;
 
 public class temp{
-    public static void main(String[] args) {
-        System.out.println("Enter Anything ");
-        Scanner abhishek = new Scanner(System.in);
-        String input =abhishek.nextLine();
-        System.out.println("you enterd : " +input);
+
     }
 }
