b42275238298139893c536fd85ba3e04b2b25f00
Abhishek Pratap Singh <84613735+i-apsingh@users.noreply.github.com>
2024-04-21 17:29:20 +0530

    commit

---
+++ arrays.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class arrays {
+    public static void main (String args[]){
+        System.out.println("apple");
+
+/*
+// input output and updation in array and find length of aaray
+        int marks[] = new int [100];
+        Scanner apple = new Scanner(System.in);
+        marks[0]=apple.nextInt();
+        marks[1]=apple.nextInt();
+        marks[2]=apple.nextInt();
+
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+
+        //updation in marks
+        marks[2]=100;
+        marks[2]=marks[2]+1;
+        System.out.println(marks[2]); // updated marks
+
+        //length of array
+        System.out.println("length of array : "+marks.length);
+*/
+
+/* 
+// creation of array
+        int marks[]=new int [50];
+        // arry with size of 50 
+
+        int numbers[]={1, 2, 3};
+        // arrry with known valur which is store later in it
+
+        String fruits[]={"apple","ball","cat"};
+        // string type array
+*/
+    }
+}
