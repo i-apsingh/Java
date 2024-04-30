3fd352e4800d481877725ccd5eec131f8cb4d5cb
Abhishek Pratap Singh <84613735+i-apsingh@users.noreply.github.com>
2024-04-30 16:13:57 +0530

    commit

--- sorting.java
+++ sorting.java
@@ -1,6 +1,36 @@
 public class sorting {
 
+    // selection sort
+    public static void selectionsort(int arr[]){
+        for (int i=0; i<arr.length-1; i++){ // outer loop for count turn
+            int minpos =i; // min positon of element which is current position
+            for (int j=i+1;j<arr.length; j++){ // inner loop
+                if (arr[minpos]>arr[j]){ // compare element 
+                    minpos=j; // update minimum position
+                }
+            }
+            //swap 
+            int temp =arr[minpos];
+            arr[minpos] =arr[i];
+            arr[i]=temp;
+        }
+    }
+
+    public static void printarr(int arr[]){
+        for (int i=0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        int arr[]={5,4,1,3,2};
+        selectionsort(arr);
+        printarr(arr);
+    }
+
     //bubble sort
+    /* 
     public static void bubblesort(int arr[]){
         for (int turn=0; turn<arr.length-1; turn++){
             for(int j=0; j<arr.length-1-turn; j++){
@@ -26,5 +56,6 @@ public class sorting {
         bubblesort(arr);
         printarr(arr);
     }
+*/
 
 }
