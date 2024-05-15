d8a4de13e45c4d908333cce74f9a50ae2a129b51
Abhishek Pratap Singh <84613735+i-apsingh@users.noreply.github.com>
2024-05-15 19:10:51 +0530

    commit

--- bitmanuplation.java
+++ bitmanuplation.java
@@ -1,6 +1,29 @@
 public class bitmanuplation {
     public static void main(String args []){
+
+
+
+        /* 
+        
+        //binary and
         System.out.println((5 & 6) );
+
+        //binary or 
+        System.out.println(5 | 6);
+
+        //binary xor
+        System.out.println(5^6);
+
+        //one's complement
+        System.out.println(~5);
+
+        // binary left shift by 2
+        System.out.println(5<<2);
+
+        // binary right shift by 1
+        System.out.println(6>>1);
+
+        */
     }
     
 }
