8edcf81ed6dd7bf2a79cbae25c67f0b815eb9dd4
Abhishek Pratap Singh <84613735+i-apsingh@users.noreply.github.com>
2024-05-15 18:20:01 +0530

    commit

--- strings.java
+++ strings.java
@@ -3,6 +3,15 @@ public class strings {
 
     public static void main(String[] args) {
 
+        // print alphabet from a to z using string builder
+
+        StringBuilder sb = new StringBuilder("");
+        for (char ch='a'; ch<='z';ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb);
+        System.out.println(sb.length());
+        
         /*
         // String comparison
         String s1 ="tony";
